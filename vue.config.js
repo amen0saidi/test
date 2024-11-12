44d5d853bf893d3dae6efa4a860431a071f37a0b
amen0saidi <amen66saidi@gmail.com>
2024-11-12 18:45:36 +0100

    Fixed issues and staged necessary files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/test/'  // Replace 'test' with your repository name if different
 });
